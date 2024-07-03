e0a0ca345b6962dcddef436a93e55824cda65903
praneeth123P <gorasapraneethswami5@gmail.com>
2024-07-03 19:34:24 +0530

    Add files via upload

---
+++ collections/Student.java
@@ -0,0 +1,12 @@
+package collections;
+import java.util.*;
+import java.io.*;
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ArrayList <String> a = new ArrayList <String>();
+a.add("praneeth");
+	}
+
+}
